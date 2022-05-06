b27c394714425aa709d62ec6cb22131de75b567d
josephenoch <enochjoseph01@gmail.com>
2022-05-06 21:55:59 +0100

    :lipstick: started working on the signup page

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,7 +7,7 @@ export const Layout = ({children}) => {
         <div className="flex justify-around z-10 shadow-md bg-white fixed top-0 shadow-black-900 w-full font-oswald">
             <div>
                 <span className="  text-2xl p-4 inline-block text-blue-400">Devs<span className='text-blue-900'>Blog</span></span>
-                <input placeholder="Search..." className="w-96 font-thin h-content border-[1px] border-gray-400 rounded-md focus:ring-blue-400 focus:outline-none focus:ring-1 p-1"/>
+                <input placeholder="Search..." className="w-96 font-thin h-content border-[1px] border-gray-400 rounded-md focus:ring-blue-400 focus:outline-none focus:ring-1 p-2"/>
             </div>
             <div className="space-y-3">
                 <Link href="/">
@@ -22,8 +22,13 @@ export const Layout = ({children}) => {
                 </Link>
             </div>
         </div>
-        <div className=" bg-gray-100">
+        <div className=" bg-gray-100 w-full font-oswald pb-5 pt-20">
             {children}
+        </div>
+        <div className="flex flex-col items-center justify-center w-full h-52 bg-gray-200 font-oswald">
+            <h1 className="mb-2">DEV Community — A constructive and inclusive social network for software developers. With you every step of your journey.</h1>
+            <h1>Built on Forem — the open source software that powers DEV and other inclusive communities.</h1>
+            <h1>Made with love and Ruby on Rails. DEV Community © 2016 - 2022.</h1>
         </div>
     </>
   )
--- pages/signup.jsx
+++ pages/signup.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const SignUp = () => {
   return (
-    <div>SignUp</div>
+    <div className=" w-full flex justify-center items-center ">
+        <div className="bg-white w-[40%] rounded-md max-w-[40%] h-[400px] p-2">
+            Hi
+        </div>
+    </div>
   )
 }
 
